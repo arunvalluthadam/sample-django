e05c9acdc49b180971280d9f2b5802acfc453c70
Arun V Jose <arunvalluthadam@gmail.com>
2015-06-12 08:36:52 +0530

    change settings base app

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -49,7 +49,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_sample',
-    'lineage',
+    # 'lineage',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,7 +57,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     # 'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
