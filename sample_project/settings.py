cab7eaa64d3ed767abe3f4e7e3252faba57655d5
Arun V Jose <arunvalluthadam@gmail.com>
2015-06-12 09:35:36 +0530

    add django djangoappengine toolbox

--- sample_project/settings.py
+++ sample_project/settings.py
@@ -1,3 +1,5 @@
+from djangoappengine.settings_base import *
+
 """
 Django settings for sample_project project.
 
@@ -42,6 +44,8 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 # Application definition
 
 INSTALLED_APPS = (
+    'djangoappengine',
+    'djangotoolbox',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -56,8 +60,8 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
